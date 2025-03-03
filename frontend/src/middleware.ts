990a4a947d6e62c53a5eab30dd08b8b80eaa3c03
leuzaoo <dev.lcostaoliveira@gmail.com>
2025-03-03 12:03:08 -0300

    Updated middleware logic

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,9 +1,27 @@
 import { NextResponse, type NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url));
+export function middleware(req: NextRequest) {
+  const token = req.cookies.get("password-manager")?.value;
+
+  const isAuthPage =
+    req.nextUrl.pathname === "/login" || req.nextUrl.pathname === "/signup";
+
+  if (token && isAuthPage) {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
+  const protectedRoute = ["/dashboard/:path*"];
+
+  if (!token && protectedRoute) {
+    const callbackUrl = encodeURIComponent(req.nextUrl.pathname);
+    return NextResponse.redirect(
+      new URL(`/login?callbackUrl=${callbackUrl}`, req.url),
+    );
+  }
+
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: "/dashboard",
+  matcher: "/dashboard/:path*",
 };
