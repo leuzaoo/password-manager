a47a80ed94b435f904befc1db8c0b8d3502e612d
leuzaoo <dev.lcostaoliveira@gmail.com>
2025-03-03 15:30:17 -0300

    Updated pages and components

--- frontend/src/app/components/common/button/index.tsx
+++ frontend/src/app/components/common/button/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 
-type Props = {
+interface MainButtonProps {
   children: string;
   type?: "submit" | "button" | "reset";
-};
+  onClick?: () => void;
+}
 
-const MainButton = ({ children, type = "button" }: Props) => {
+const MainButton = ({
+  children,
+  type = "button",
+  onClick,
+}: MainButtonProps) => {
   return (
     <button
       type={type}
+      onClick={onClick}
       className="w-full cursor-pointer rounded-lg bg-neutral-100 py-4 text-neutral-900"
     >
       {children}
