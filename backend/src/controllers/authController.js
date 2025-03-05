85551c9d32f55a854389622bfbd1e10324c31cc2
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-03-05 11:53:38 -0300

    Fix line error

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -6,9 +6,9 @@ import generateToken from "./../config/generateToken.js";
 export async function signup(req, res) {
   const { email, password } = req.body;
 
-if(!password || typeof password !== "string"){
-res.status(400).json({message: "Insira um tipo válido de senha."})
-}
+  if (!password || typeof password !== "string") {
+    res.status(400).json({ message: "Insira um tipo válido de senha." });
+  }
 
   if (password.length < 6) {
     return res.status(400).json({ message: "Senha deve conter 6 caracteres." });
