75392c17ebe012f1a3d5786610256c35675bd653
leuzaoo <leuzaoocontato@gmail.com>
2025-03-06 02:42:49 -0300

    Update main button component

--- frontend/src/app/components/common/button/index.tsx
+++ frontend/src/app/components/common/button/index.tsx
@@ -4,18 +4,23 @@ interface MainButtonProps {
   children: string;
   type?: "submit" | "button" | "reset";
   onClick?: () => void;
+  className?: string;
 }
+
+const mainStyle =
+  "w-full cursor-pointer rounded-lg bg-neutral-100 py-4 text-neutral-900 ";
 
 const MainButton = ({
   children,
   type = "button",
   onClick,
+  className,
 }: MainButtonProps) => {
   return (
     <button
       type={type}
       onClick={onClick}
-      className="w-full cursor-pointer rounded-lg bg-neutral-100 py-4 text-neutral-900"
+      className={`${mainStyle + className}`}
     >
       {children}
     </button>
