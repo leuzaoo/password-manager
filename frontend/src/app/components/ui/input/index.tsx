bcd62bd537da979dea2461c1053261813d8a76c2
leuzaoo <leuzaoocontato@gmail.com>
2025-03-07 22:19:26 -0300

    Updated input component

--- frontend/src/app/components/ui/input/index.tsx
+++ frontend/src/app/components/ui/input/index.tsx
@@ -5,17 +5,27 @@ type Props = {
   placeholder: string;
   type: string;
   value: string;
+  className?: string;
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const MainInput = ({ label, placeholder, type, value, onChange }: Props) => {
+const mainStyle = "max-w-80 rounded-lg border border-neutral-600 py-4 pl-2 ";
+
+const MainInput = ({
+  label,
+  placeholder,
+  type,
+  value,
+  className,
+  onChange,
+}: Props) => {
   return (
     <>
       <label>{label}</label>
       <input
         onChange={onChange}
         value={value}
-        className="w-80 rounded-lg border border-neutral-600 py-4 pl-2"
+        className={mainStyle + className}
         type={type}
         placeholder={placeholder}
       />
