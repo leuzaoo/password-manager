b700698d01e5410a3a6f137f883223fa8ab23a3b
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-03-09 00:08:37 -0300

    Updated last updates component

--- frontend/src/app/components/ui/last-updates/index.tsx
+++ frontend/src/app/components/ui/last-updates/index.tsx
@@ -1,8 +1,18 @@
 const LastUpdatesSection = () => {
   return (
-    <>
-      <h1>Last updates section v1</h1>
-    </>
+    <div className="h-full w-full border-l border-white/10 p-4">
+      <h1 className="text-2xl">Últimas atualizações</h1>
+      <div className="bg-primary-alert text-primary-dark mt-5 w-full rounded-lg p-3">
+        <h3 className="text-lg font-semibold">Tabela visual</h3>
+        <hr className="opacity-20" />
+        <p className="mt-3">
+          Foi adicionado uma tabela para visualização das senhas na página de
+          visão geral. Nesta tabela, o usuário será capaz de visualizar e editar
+          suas senhas adicionadas.
+        </p>
+        <span className="mt-3 text-xs font-light">09.03.2024</span>
+      </div>
+    </div>
   );
 };
 
