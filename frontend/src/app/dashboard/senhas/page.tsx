268b5ef02492e5eab8acfa5b4c8c0f075ef8c5b6
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-03-10 13:14:48 -0300

    Created my-passwords users page

--- frontend/src/app/dashboard/senhas/page.tsx
+++ frontend/src/app/dashboard/senhas/page.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import PasswordTable from "@/app/components/ui/table";
 
 const PasswordsPage = () => {
-  return <div>PasswordsPage</div>;
+  return (
+    <div>
+      <h2 className="text-2xl font-medium">Minhas senhas</h2>
+
+      <PasswordTable />
+    </div>
+  );
 };
 
 export default PasswordsPage;
