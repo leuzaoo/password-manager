cb2a1eb3f5314ca8878952414762a86e2a076c51
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-03-10 10:55:03 -0300

    Updated the way that backend was connected

--- backend/src/controllers/passwordsController.js
+++ backend/src/controllers/passwordsController.js
@@ -1,4 +1,4 @@
-import pool from "../config/dbConfig.js"
+import pool from "../config/dbConfig.js";
 import jwt from "jsonwebtoken";
 
 export async function addPassword(req, res) {
@@ -7,7 +7,7 @@ export async function addPassword(req, res) {
   const token = req.cookies["password-manager"];
 
   if (!token) {
-    return res.status(401).json({message: "Usuário precisa estar logado."});
+    return res.status(401).json({ message: "Usuário precisa estar logado." });
   }
 
   try {
@@ -15,16 +15,24 @@ export async function addPassword(req, res) {
     const userId = decoded.userId;
 
     if (!userId) {
-      return res.status(401).json({message: "Token inválido."});
+      return res.status(401).json({ message: "Token inválido." });
     }
 
-    if (!platform || !login || !password) {
-      return res.status(400).json({message: "Todos os campos devem ser preenchidos."});
+    if (!platform) {
+      return res.status(400).json({ message: "Campo de plataforma vazio." });
+    }
+
+    if (!login) {
+      return res.status(400).json({ message: "Campo de login vazio." });
+    }
+
+    if (!password) {
+      return res.status(400).json({ message: "Campo de senha vazio." });
     }
 
     const newPassword = await pool.query(
       "INSERT INTO passwords (user_id, platform, login, password) VALUES ($1, $2, $3, $4) RETURNING id, platform, login",
-      [userId, platform, login, password]
+      [userId, platform, login, password],
     );
 
     res.status(201).json({
@@ -37,6 +45,6 @@ export async function addPassword(req, res) {
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({error: error.message});
+    res.status(500).json({ error: error.message });
   }
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 import cookieParser from "cookie-parser";
+import pool from "./config/dbConfig.js";
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
@@ -6,7 +7,8 @@ import cors from "cors";
 
 import passwordsRoutes from "./routes/passwordsRoutes.js";
 import authRoutes from "./routes/authRoutes.js";
-import pool from "./config/dbConfig.js";
+
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
@@ -15,7 +17,7 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     credentials: true,
-  })
+  }),
 );
 app.use(helmet());
 app.use(morgan("dev"));
@@ -23,8 +25,21 @@ app.use(morgan("dev"));
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/passwords", passwordsRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  pool.connect();
-  console.log(`Server running on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
+
+server.on("error", (err) => {
+  if (err.code === "EADDRINUSE") {
+    console.error(`Port ${PORT} in use. Try another one.`);
+    process.exit(1);
+  } else {
+    console.error("Server error: ", err.message);
+  }
+});
+
+process.on("SIGINT", async () => {
+  console.log("Closing server...");
+  await pool.end();
+  process.exit();
 });
