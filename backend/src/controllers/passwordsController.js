fae6edf970923215816829742c9c90ce49e2f634
dev-leuzaoo <ofurtivo2019@gmail.com>
2025-03-12 14:39:14 -0300

    Updated backend logic functions

--- backend/src/controllers/passwordsController.js
+++ backend/src/controllers/passwordsController.js
@@ -13,28 +13,27 @@ export async function addPassword(req, res) {
         .json({ message: "Todos os campos são obrigatórios." });
     }
 
+    let client;
+
     try {
-      const client = await pool.connect();
+      client = await pool.connect();
 
-      try {
-        const newPassword = await client.query(
-          "INSERT INTO passwords (user_id, platform, login, password) VALUES ($1, $2, $3, $4) RETURNING id, platform, login",
-          [userId, platform, login, password],
-        );
+      const newPassword = await client.query(
+        "INSERT INTO passwords (user_id, platform, login, password) VALUES ($1, $2, $3, $4) RETURNING id, platform, login",
+        [userId, platform, login, password],
+      );
 
-        res.status(201).json({
-          message: "Senha salva com sucesso.",
-          password: newPassword.rows[0],
-        });
-      } catch (error) {
-        console.error("Erro ao salvar senha no banco:", error);
-        res.status(500).json({ message: error.message });
-      } finally {
+      res.status(201).json({
+        message: "Senha salva com sucesso.",
+        password: newPassword.rows[0],
+      });
+    } catch (error) {
+      console.error("Erro ao salvar senha no banco:", error);
+      res.status(500).json({ message: error.message });
+    } finally {
+      if (client) {
         client.release();
       }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.message });
     }
   });
 }
@@ -51,38 +50,36 @@ export async function updatePassword(req, res) {
         .json({ message: "Todos os campos são obrigatórios." });
     }
 
+    let client;
+
     try {
-      const client = await pool.connect();
+      client = await pool.connect();
 
-      try {
-        const result = await client.query(
-          `UPDATE passwords
-             SET platform = $1, login = $2, password = $3
-             WHERE id = $4 AND user_id = $5
-               RETURNING id, platform, login, password`,
-          [platform, login, password, id, userId],
-        );
+      const result = await client.query(
+        `UPDATE passwords
+           SET platform = $1, login = $2, password = $3
+           WHERE id = $4 AND user_id = $5
+             RETURNING id, platform, login, password`,
+        [platform, login, password, id, userId],
+      );
 
-        if (result.rowCount === 0) {
-          return res.status(404).json({
-            message:
-              "Senha não encontrada ou você não tem permissão para editá-la.",
-          });
-        }
-
-        res.status(200).json({
-          message: "Senha atualizada com sucesso.",
-          password: result.rows[0],
+      if (result.rowCount === 0) {
+        return res.status(404).json({
+          message:
+            "Senha não encontrada ou você não tem permissão para editá-la.",
         });
-      } catch (error) {
-        console.error("Erro ao atualizar a senha: ", error);
-        res.status(500).json({ message: error.message });
-      } finally {
-        client.release();
       }
+
+      res.status(200).json({
+        message: "Senha atualizada com sucesso.",
+        password: result.rows[0],
+      });
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.message });
+      console.error("Erro ao atualizar a senha: ", error);
+      res.status(500).json({ message: error.message });
+    } finally {
+      if (client) {
+      }
     }
   });
 }
@@ -96,33 +93,32 @@ export async function deletePassword(req, res) {
       return res.status(400).json({ message: "ID da senha é obrigatório." });
     }
 
+    let client;
+
     try {
-      const client = await pool.connect();
+      client = await pool.connect();
 
-      try {
-        const passwordCheck = await client.query(
-          "SELECT * FROM passwords WHERE id = $1 AND user_id = $2",
-          [id, userId],
-        );
+      const passwordCheck = await client.query(
+        "SELECT * FROM passwords WHERE id = $1 AND user_id = $2",
+        [id, userId],
+      );
 
-        if (passwordCheck.rows.length === 0) {
-          return res
-            .status(404)
-            .json({ message: "Senha não encontrada ou não pertence a você." });
-        }
+      if (passwordCheck.rows.length === 0) {
+        return res
+          .status(404)
+          .json({ message: "Senha não encontrada ou não pertence a você." });
+      }
 
-        await client.query("DELETE FROM passwords WHERE id = $1", [id]);
+      await client.query("DELETE FROM passwords WHERE id = $1", [id]);
 
-        res.status(200).json({ message: "Senha excluída com sucesso." });
-      } catch (error) {
-        console.error("Erro ao deletar senha no banco:", error);
-        res.status(500).json({ message: error.message });
-      } finally {
+      res.status(200).json({ message: "Senha excluída com sucesso." });
+    } catch (error) {
+      console.error("Erro ao deletar senha no banco:", error);
+      res.status(500).json({ message: error.message });
+    } finally {
+      if (client) {
         client.release();
       }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.message });
     }
   });
 }
@@ -131,8 +127,10 @@ export async function getAllPassword(req, res) {
   authenticateUser(req, res, async () => {
     const userId = req.userId;
 
+    let client;
+
     try {
-      const client = await pool.connect();
+      client = await pool.connect();
 
       const data = await client.query(
         "SELECT * FROM passwords WHERE user_id = $1",
@@ -142,6 +140,10 @@ export async function getAllPassword(req, res) {
     } catch (error) {
       console.error("Erro ao buscar senhas:", error);
       res.status(500).json({ message: error.message });
+    } finally {
+      if (client) {
+        client.release();
+      }
     }
   });
 }
